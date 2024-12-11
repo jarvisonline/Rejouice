cd9d97ae8c1e1ae179c738ccd8306b9bfea57e98
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-12-11 16:02:11 +0530

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+var page1Content = document.querySelector("#page1-content");
+
+var cursor = document.querySelector("#cursor");
+
+page1Content.addEventListener("mousemove", function (dets) {
+  gsap.to("#cursor", {
+    x: dets.x,
+    y: dets.y,
+  });
+});
